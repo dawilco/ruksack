166f6cb0eadb23b598342c59cdbd486282acde59
Dylan Wilcox <dawilco@g.clemson.edu>
2021-07-12 20:47:34 -0400

    Static homepage into app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
 class StaticPagesController < ApplicationController
-	def home; end
+  layout 'static'
+
+  def home; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :events
-  resources :dashboard
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+
+  # admin
+  namespace :admin do
+    resources :dashboard
+  end
 end
