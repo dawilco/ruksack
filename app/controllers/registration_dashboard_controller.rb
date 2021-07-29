7d79018e9f10c117980b09e9ff0ec3e3b014245b
dawilco <dawilco@g.clemson.edu>
2021-07-28 21:03:48 -0400

    somewhat styled cards for events

--- app/controllers/registration_dashboard_controller.rb
+++ app/controllers/registration_dashboard_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationDashboardController < ApplicationController
   layout 'registration'
   before_action :set_events, only: %i[dashboard]
+  before_action :set_organization, only: %i[dashboard]
 
   def dashboard; end
 
@@ -10,4 +11,7 @@ class RegistrationDashboardController < ApplicationController
     @events = Organization.find_by(url_key: params[:organization_name]).events
   end
 
+  def set_organization
+    @organization = Organization.find_by(url_key: params[:organization_name])
+  end
 end
